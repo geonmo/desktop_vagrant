828cd868bf7371ddfedb517b7ab588c4a16d988a
Geonmo Ryu <geonmo@kisti.re.kr>
2020-10-15 02:01:42 +0000

    Add credention.

--- 00_git_setup.sh
+++ 00_git_setup.sh
@@ -1,3 +1,4 @@
 git config --global user.name "Geonmo Ryu"
 git config --global user.email geonmo@kisti.re.kr
 git config --global push.default simple
+git config --global credential.helper 'store --file ~/.my-credentials'
