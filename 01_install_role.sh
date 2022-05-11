92f5176337a3fba1c7f0f9e4eac0aaf3cb4b2f2e
Geonmo Ryu <geonmo@kisti.re.kr>
2022-05-11 13:45:12 +0900

    Update 01_install_role.sh

--- 01_install_role.sh
+++ 01_install_role.sh
@@ -2,6 +2,10 @@
 #sudo yum update -y
 #sudo yum update -y ansible
 
+## For postgresql ansible module,
+sudo yum install python-psycopg2
+
+
 ansible-galaxy collection install community.general
 git submodule init
 git submodule update
